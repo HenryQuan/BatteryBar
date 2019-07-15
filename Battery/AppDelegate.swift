3bd5296d8c0495106bd1deda33e09cbd4c87bc52
Yiheng (2011) <development.henryquan@gmail.com>
2019-07-15 20:23:20 +1000

    show hello world

--- Battery/AppDelegate.swift
+++ Battery/AppDelegate.swift
@@ -11,10 +11,14 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
+    // Define a menu
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.variableLength)
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        // Show "Hello World"
+        if let button = statusItem.button {
+            button.title = "Hello World"
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
