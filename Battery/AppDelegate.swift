84c59138fdaa7512bf5970a21bd364965184c1f6
Yiheng (2011) <development.henryquan@gmail.com>
2019-07-15 20:19:32 +1000

    Initial Commit

---
+++ Battery/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Battery
+//
+//  Created by Yiheng Quan on 15/7/19.
+//  Copyright © 2019 Yiheng Quan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
