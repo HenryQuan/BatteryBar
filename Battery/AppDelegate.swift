1128b8fa2e52656c2bda4d6bc2d2ed9d503e6cb4
Yiheng (2011) <development.henryquan@gmail.com>
2019-07-16 22:45:51 +1000

    build 2

--- Battery/AppDelegate.swift
+++ Battery/AppDelegate.swift
@@ -36,15 +36,39 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     /// Get time remain for this machine
     /// Returns a string like 2:12 remaning
     func updateTimeRemain() -> String {
-        var remain = "??? remaining";
+        var remain = "- no estimate -";
         // pmset -g batt
         let output = runCommand(cmd: "/usr/bin/pmset", args: "-g", "batt").output
-        let time = output[1].split(separator: ";").last
-        if time != nil {
-            remain = String(time!)
+        
+        // Find string with format 1:23
+        let matches = self.matches(for: "[0-9]+:[0-9]+", in: output[1])
+        if matches.count > 0 {
+            remain = matches[0]
+        }
+        
+        // Add different emoji to distinguish different mode
+        if output.joined().contains("discharg") {
+            remain += "🔋"
+        } else {
+            remain += "⚡️"
         }
         
         return remain
+    }
+    
+    /// From https://stackoverflow.com/questions/27880650/swift-extract-regex-matches
+    /// Regex
+    func matches(for regex: String, in text: String) -> [String] {
+        do {
+            let regex = try NSRegularExpression(pattern: regex)
+            let results = regex.matches(in: text, range: NSRange(text.startIndex..., in: text))
+            return results.map {
+                String(text[Range($0.range, in: text)!])
+            }
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+            return []
+        }
     }
     
     /// From https://stackoverflow.com/questions/29514738/get-terminal-output-after-a-command-swift
